1e31452ae0d31e2fe2e64893c0ee4af7a233a759
singhsanket143 <singhsanket143@gmail.com>
2019-05-27 23:29:25 +0530

    Added event listener

--- client/main.js
+++ client/main.js
@@ -10,3 +10,15 @@ Template.body.helpers({
     return Resolutions.find({});
   }
 });
+
+Template.body.events({
+  "submit .new-resolution": function(event) {
+    var title = event.target.title.value;
+    Resolutions.insert({
+      title: title,
+      createdAt: new Date()
+    });
+    event.target.title.value = "";
+    return false;
+  }
+});
