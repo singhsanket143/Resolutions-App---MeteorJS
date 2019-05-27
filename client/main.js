22655ab7b473efe4cb8fa974c0f88ec1505fe10d
singhsanket143 <singhsanket143@gmail.com>
2019-05-28 01:26:25 +0530

    Added temporary ui state using Session instance

--- client/main.js
+++ client/main.js
@@ -1,13 +1,26 @@
 import { Template } from "meteor/templating";
 import { Resolutions } from "../imports/api/resolution";
+import { ReactiveDict } from "meteor/reactive-dict";
 import { ReactiveVar } from "meteor/reactive-var";
 
 import "./main.html";
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
   resolutions: function() {
+    const instance = Template.instance();
     console.log(Resolutions.find({}).fetch());
-    return Resolutions.find({});
+    if (instance.state.get("hideFinished")) {
+      return Resolutions.find({ checked: { $ne: true } });
+    } else {
+      return Resolutions.find({});
+    }
+  },
+  hideFinished: function() {
+    const instance = Template.instance();
+    return instance.state.get("hideFinished");
   }
 });
 
@@ -20,11 +33,15 @@ Template.body.events({
     });
     event.target.title.value = "";
     return false;
+  },
+  "change .hide-finished input": function(event, instance) {
+    console.log(event.target.checked);
+    instance.state.set("hideFinished", event.target.checked);
   }
 });
 
 Template.resolution.events({
-  "click .toggle-checked": function() {
+  "change .toggle-checked": function() {
     Resolutions.update(this._id, { $set: { checked: !this.checked } });
   },
   "click .delete": function() {
